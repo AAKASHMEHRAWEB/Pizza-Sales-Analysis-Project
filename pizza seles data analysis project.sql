7cc32c18d30fab122c66394be480123689f77f83
AAKASHMEHRAWEB <akumarmehra1997@gmail.com>
2025-10-27 14:08:50 +0530

    Enhance pizza sales data analysis with new queries
    Added SQL queries to analyze pizza sales data, including revenue contribution by pizza type, cumulative revenue over time, and top ordered pizza types by category.

--- pizza seles data analysis project.sql
+++ pizza seles data analysis project.sql
@@ -148,5 +148,58 @@ GROUP BY pizza_types.name
 ORDER BY revenue DESC
 LIMIT 5;
 
+-- Q 11 
+-- Calculate the percentage contribution of each pizza type to total revenue.
 
+SELECT 
+    pizza_types.category,
+    ROUND(
+        SUM(order_details.quantity * pizzas.price) / 
+        (
+            SELECT 
+                ROUND(SUM(od.quantity * p.price), 2) AS total_sales
+            FROM 
+                order_details AS od 
+                JOIN pizzas AS p ON od.pizza_id = p.pizza_id
+        ) * 100,
+        2
+    ) AS revenue
+FROM 
+    pizza_types
+    JOIN pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+    JOIN order_details ON order_details.pizza_id = pizzas.pizza_id
+GROUP BY 
+    pizza_types.category
+ORDER BY 
+    revenue DESC;
+
+-- Q - 12
+-- Analyze the cumulative revenue generated over time.
+
+select order_date,
+sum(revenue) over(order by order_date) as cum_revenue
+from 
+(select orders.order_date,
+sum(order_details.quantity * pizzas.price)as revenue
+from order_details join pizzas
+on order_details.pizza_id = pizzas.pizza_id
+join orders
+on orders.order_id = order_details.order_id
+group by orders.order_date) as sales ;
+
+-- Q 13
+-- Determine the top 3 most ordered pizza types based on revenue for each pizza category.
+
+select name, revenue from 
+(select category, name, revenue,
+rank() over(partition by category order by revenue desc)as rn 
+from 
+(select pizza_types.category, pizza_types.name,
+sum((order_details.quantity)*pizzas.price)as revenue
+from pizza_types join pizzas
+on pizza_types.pizza_type_id = pizzas.pizza_type_id
+join order_details 
+on order_details.pizza_id = pizzas.pizza_id
+group by pizza_types.category, pizza_types.name) as a) as b
+where rn <= 3;
 
